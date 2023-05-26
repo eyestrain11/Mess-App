565d96a0804a57ef88d7ae46f586c06f7d4c67c7
smolguy22 <shaurya.student.eee21@itbhu.ac.in>
2023-05-26 23:14:28 +0530

    Added Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import BasicExample from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nav" style={{backgroundImage:`url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fwebsite%2520background%2F&psig=AOvVaw3B3HqEQ_dObVyBcSiTxGSZ&ust=1685196933876000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJiB8NmXk_8CFQAAAAAdAAAAABAD")`}}>
+      <BasicExample/>
     </div>
+    
   );
 }
 
