bb9da6d29c07155c41ba5474b1776aa2f1d7bcca
smolguy22 <shaurya.student.eee21@itbhu.ac.in>
2023-05-29 16:32:50 +0530

    google button

--- frontend/src/components/pages/SignUp.js
+++ frontend/src/components/pages/SignUp.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import '../../App.css';
+import { useEffect } from 'react';
+import jwt_decode from 'jwt-decode';
 
-export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+
+
+
+ function SignUp() {
+
+    // const [user,setUser] = useState({});
+
+function handleCallbackResponse(response){
+    console.log("Encoded JWT ID token :" + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    // setUser(userObject);
 }
+
+useEffect(()=>{
+  /*global google*/
+  google.accounts.id.initialize({
+    client_id:"802699845079-f88eqoj2naua7selgj0g5dg8vfjm3or5.apps.googleusercontent.com",
+    callback: handleCallbackResponse
+    // hosted_domain:'itbhu.ac.in'
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("signInDiv"),
+    {theme: "outline",size:"large"}
+  );
+  google.accounts.id.prompt();
+},[]);
+
+  return (
+    <div id='signInDiv'>googleLogin</div>
+  )
+}
+export default SignUp;
